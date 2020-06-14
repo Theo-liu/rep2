bf5accd4e0c5cef64fd344d1498aca94f8ac4175
theo <theo@itcast.cn>
2020-06-14 14:30:08 +0800

    第一次提交

---
+++ Demo2.java
@@ -0,0 +1,20 @@
+package practice.test1;
+
+import java.io.FileNotFoundException;
+
+/*
+需求：定义一个可以接受字符串的方法，字符不包含a.txt抛出异常
+ */
+public class Demo2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        String string = getString("b.txt");
+        System.out.println(string);
+
+    }
+    public static String getString(String s)throws FileNotFoundException {
+        if (!s.equals("a/.txt")) {
+            throw new FileNotFoundException("文件不存在！");
+        }
+        return s;
+    }
+}
